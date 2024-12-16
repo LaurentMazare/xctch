e92bcb5bccf8410492fd1b8696e943b74831df72
laurent <laurent.mazare@gmail.com>
2024-12-16 10:51:10 +0100

    Cleanup the tokenizer code.

--- xctch-core/examples/nanogpt.rs
+++ xctch-core/examples/nanogpt.rs
@@ -2,12 +2,26 @@ use anyhow::Result;
 use std::collections::HashMap;
 use std::io::Write;
 
+struct Tokenizer {
+    vocab: HashMap<usize, String>,
+}
+
+impl Tokenizer {
+    fn token_str(&self, token_id: usize) -> String {
+        self.vocab.get(&token_id).map_or("UNK", |v| v.as_str()).replace('Ġ', " ").replace("Ċ", "\n")
+    }
+
+    fn load<P: AsRef<std::path::Path>>(p: P) -> Result<Self> {
+        let json_data = std::fs::read_to_string(p)?;
+        let vocab: HashMap<String, usize> = serde_json::from_str(&json_data)?;
+        let vocab: HashMap<usize, String> = vocab.into_iter().map(|(k, v)| (v, k)).collect();
+        Ok(Self { vocab })
+    }
+}
+
 fn main() -> Result<()> {
     xctch::et_pal_init();
-    let file_path = "scripts/nanogpt/vocab.json";
-    let json_data = std::fs::read_to_string(file_path)?;
-    let vocab: HashMap<String, usize> = serde_json::from_str(&json_data)?;
-    let vocab: HashMap<usize, String> = vocab.into_iter().map(|(k, v)| (v, k)).collect();
+    let tokenizer = Tokenizer::load("scripts/nanogpt/vocab.json")?;
 
     let program = xctch::Program::from_file("scripts/nanogpt/nanogpt.pte")?;
     let mut method = program.method("forward")?;
@@ -17,9 +31,7 @@ fn main() -> Result<()> {
     }
     let mut tokens = vec![18435i64];
     for &token in tokens.iter() {
-        let token = token as usize;
-        let token_str = vocab.get(&token).map_or("", |v| v.as_str()).replace('Ġ', " ");
-        print!("{}", token_str);
+        print!("{}", tokenizer.token_str(token as usize));
     }
     for _ in 0..20 {
         let tokens_len = tokens.len();
@@ -33,8 +45,7 @@ fn main() -> Result<()> {
         let logits = logits.as_slice::<f32>().unwrap();
         // TODO: softmax + sampling
         let token = logits.iter().enumerate().max_by(|&(_, a), &(_, b)| a.total_cmp(b)).unwrap().0;
-        let token_str = vocab.get(&token).map_or("", |v| v.as_str()).replace('Ġ', " ");
-        print!("{}", token_str);
+        print!("{}", tokenizer.token_str(token as usize));
         let _ = std::io::stdout().flush();
         tokens.push(token as i64)
     }
