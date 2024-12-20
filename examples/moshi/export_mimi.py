8bb103f9c970aa3c724cbea4b969d209edade9cb
laurent <laurent.mazare@gmail.com>
2024-12-20 17:33:13 +0100

    Script for exporting the mimi encoder.

---
+++ examples/moshi/export_mimi.py
@@ -0,0 +1,49 @@
+import argparse
+import torch
+from torch.export import export, ExportedProgram
+from executorch.exir import EdgeProgramManager, ExecutorchBackendConfig, to_edge
+
+from huggingface_hub import hf_hub_download
+from moshi.models import loaders
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mimi-weight", type=str, help="Path to a local checkpoint file for Mimi.")
+    parser.add_argument("--hf-repo", type=str, default=loaders.DEFAULT_REPO,
+                        help="HF repo to look into, defaults Moshiko. "
+                             "Use this to select a different pre-trained model.")
+    parser.add_argument("--device", type=str, default="cpu")
+    args = parser.parse_args()
+
+    if args.mimi_weight is None:
+        args.mimi_weight = hf_hub_download(args.hf_repo, loaders.MIMI_NAME)
+    mimi = loaders.get_mimi(args.mimi_weight, args.device)
+    print("==== MIMI MODEL ====")
+    print(mimi)
+
+    sample_pcm = torch.zeros((1, 1, 1920), dtype=torch.float).to(args.device)
+
+    print("==== RUN MODEL ====")
+    _out = mimi.encoder(sample_pcm)
+    print(_out.shape)
+
+    print("==== ATEN DIALECT ====")
+    aten_dialect: ExportedProgram = export(mimi.encoder, (sample_pcm,))
+    print(aten_dialect)
+    edge_program: EdgeProgramManager = to_edge(aten_dialect)
+
+    print("==== EDGE PROGRAM ====")
+    print(edge_program)
+
+    executorch_program: exir.ExecutorchProgramManager = edge_program.to_executorch(
+        ExecutorchBackendConfig(
+            passes=[],  # User-defined passes
+        )
+    )
+
+    with open("mimi.pte", "wb") as file:
+        file.write(executorch_program.buffer)
+
+with torch.no_grad():
+    main()
