dff2bcc44f2b5eacdfdd830bb5b5b52d0cf39797
laurent <laurent.mazare@gmail.com>
2024-12-21 10:45:04 +0100

    Export both the mimi encoder and decoder.

--- examples/moshi/export_mimi.py
+++ examples/moshi/export_mimi.py
@@ -1,7 +1,7 @@
 import argparse
 import torch
 from torch import nn
-from torch.export import export, ExportedProgram
+from torch.export import export, ExportedProgram, Dim
 from executorch.exir import EdgeProgramManager, ExecutorchBackendConfig, to_edge
 
 from huggingface_hub import hf_hub_download
@@ -16,6 +16,14 @@ class MimiEncoder(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.mimi.encode(x)
 
+class MimiDecoder(nn.Module):
+    def __init__(self, mimi):
+        super().__init__()
+        self.mimi = mimi
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.mimi.decode(x)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -29,33 +37,42 @@ def main():
     if args.mimi_weight is None:
         args.mimi_weight = hf_hub_download(args.hf_repo, loaders.MIMI_NAME)
     mimi = loaders.get_mimi(args.mimi_weight, args.device)
-    print("==== MIMI MODEL ====")
-    print(mimi)
+    print("mimi model loaded")
 
+    seq_len = 24000 * 10
     mimi_encoder = MimiEncoder(mimi)
+    mimi_decoder = MimiDecoder(mimi)
+    sample_pcm = torch.zeros((1, 1, seq_len), dtype=torch.float).to(args.device)
 
-    sample_pcm = torch.zeros((1, 1, 1920), dtype=torch.float).to(args.device)
+    print("running the encoder")
+    sample_codes = mimi_encoder(sample_pcm)
+    print(sample_codes.shape)
+    print("running the decoder")
+    _pcm = mimi_decoder(sample_codes)
+    print(_pcm.shape)
 
-    print("==== RUN MODEL ====")
-    _out = mimi_encoder(sample_pcm)
-    print(_out.shape)
-
-    print("==== ATEN DIALECT ====")
-    aten_dialect: ExportedProgram = export(mimi_encoder, (sample_pcm,))
-    print(aten_dialect)
-    edge_program: EdgeProgramManager = to_edge(aten_dialect)
-
-    print("==== EDGE PROGRAM ====")
-    print(edge_program)
-
-    executorch_program: exir.ExecutorchProgramManager = edge_program.to_executorch(
-        ExecutorchBackendConfig(
-            passes=[],  # User-defined passes
+    for model, inp_, filename in [
+            (mimi_encoder, sample_pcm, "mimi_encoder.pte"),
+            (mimi_decoder, sample_codes, "mimi_decoder.pte")]:
+        print("exporting to aten and edge")
+        aten_dialect: ExportedProgram = export(
+            model,
+            (inp_,),
+            # Dynamic shapes result in unresolvable constraints
+            # dynamic_shapes={"x": {2: Dim("t", min=1, max=240000)}},
         )
-    )
+        edge_program: EdgeProgramManager = to_edge(aten_dialect)
 
-    with open("mimi.pte", "wb") as file:
-        file.write(executorch_program.buffer)
+        print("exporting to executorch")
+        executorch_program: exir.ExecutorchProgramManager = edge_program.to_executorch(
+            ExecutorchBackendConfig(
+                passes=[],  # User-defined passes
+            )
+        )
+
+        print(f"writing {filename}")
+        with open(filename, "wb") as file:
+            file.write(executorch_program.buffer)
 
 with torch.no_grad():
     main()
