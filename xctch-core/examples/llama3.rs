f1ed621c6417ffb457029de2053955e28b7be8de
Laurent <laurent.mazare@gmail.com>
2024-12-22 08:08:59 +0100

    Proper sampling in the llama3 example.

--- xctch-core/examples/llama3.rs
+++ xctch-core/examples/llama3.rs
@@ -1,6 +1,10 @@
-use anyhow::Result;
+use rand::{distributions::Distribution, SeedableRng};
 use std::collections::HashMap;
 use std::io::Write;
+use xctch::{Error as E, Result};
+
+pub const TEMPERATURE: f32 = 0.6;
+pub const SEED: u64 = 4242424242424242;
 
 struct Tokenizer {
     vocab: HashMap<usize, String>,
@@ -11,10 +15,9 @@ impl Tokenizer {
         self.vocab.get(&token_id).map_or("UNK", |v| v.as_str()).replace('Ġ', " ").replace("Ċ", "\n")
     }
 
-    fn load<P: AsRef<std::path::Path>>(p: P) -> xctch::Result<Self> {
+    fn load<P: AsRef<std::path::Path>>(p: P) -> Result<Self> {
         let json_data = std::fs::read_to_string(p)?;
-        let vocab: HashMap<String, usize> =
-            serde_json::from_str(&json_data).map_err(xctch::Error::wrap)?;
+        let vocab: HashMap<String, usize> = serde_json::from_str(&json_data).map_err(E::wrap)?;
         let vocab: HashMap<usize, String> = vocab.into_iter().map(|(k, v)| (v, k)).collect();
         Ok(Self { vocab })
     }
@@ -22,6 +25,7 @@ impl Tokenizer {
 
 fn main() -> Result<()> {
     xctch::et_pal_init();
+    let mut rng = rand::rngs::StdRng::seed_from_u64(SEED);
     let vocab_path = "scripts/llama3/vocab.json";
     let tokenizer = Tokenizer::load(vocab_path).map_err(|e| e.with_path(vocab_path))?;
 
@@ -47,8 +51,17 @@ fn main() -> Result<()> {
         let logits = method.get_output(0);
         let logits = logits.as_tensor().unwrap();
         let logits = logits.as_slice::<half::bf16>().unwrap();
-        // TODO: softmax + sampling
-        let token = logits.iter().enumerate().max_by(|&(_, a), &(_, b)| a.total_cmp(b)).unwrap().0;
+        let token = if TEMPERATURE <= 0. {
+            logits.iter().enumerate().max_by(|&(_, a), &(_, b)| a.total_cmp(b)).unwrap().0
+        } else {
+            let max_logit = logits.iter().max_by(|&a, &b| a.total_cmp(b)).unwrap();
+            let mut sm: Vec<f32> =
+                logits.iter().map(|v| ((*v - *max_logit).to_f32() / TEMPERATURE).exp()).collect();
+            let sum_sm = sm.iter().sum::<f32>();
+            sm.iter_mut().for_each(|v| *v /= sum_sm);
+            let distr = rand::distributions::WeightedIndex::new(sm).map_err(E::wrap)?;
+            distr.sample(&mut rng)
+        };
         print!("{}", tokenizer.token_str(token));
         std::io::stdout().flush()?;
         tokens.push(token as i64)
