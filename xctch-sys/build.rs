eadc6873c88b7061c2b0d851575c0f933975726f
laurent <laurent.mazare@gmail.com>
2025-01-06 08:53:14 +0100

    Rework the build script.

--- xctch-sys/build.rs
+++ xctch-sys/build.rs
@@ -1,17 +1,27 @@
+const ENV_VAR: &str = "EXECUTORCH_DIR";
+const ENV_VAR_INCLUDE: &str = "EXECUTORCH_DIR_INCLUDE";
+const ENV_VAR_LIB: &str = "EXECUTORCH_DIR_LIB";
+
 fn main() {
-    let exec_dir = std::env::var("EXECUTORCH_DIR").expect("EXECUTORCH_DIR is not set");
+    let exec_dir = std::env::var(ENV_VAR).expect(&format!("{ENV_VAR} is not set"));
+    let include_dir =
+        std::env::var(ENV_VAR_INCLUDE).unwrap_or_else(|_| format!("{exec_dir}/include"));
+    let lib_dir = std::env::var(ENV_VAR_INCLUDE).unwrap_or_else(|_| format!("{exec_dir}/lib"));
 
     cxx_build::bridge("src/cxx_ffi.rs")
-        .include(format!("{exec_dir}/../"))
+        .include(include_dir)
         .std("c++17")
         .file("cpp/cxx_api.cpp")
         .compile("xctch-sys");
 
-    println!("cargo:rerun-if-env-changed=EXECUTORCH_DIR");
+    println!("cargo:rerun-if-env-changed={ENV_VAR}");
+    println!("cargo:rerun-if-env-changed={ENV_VAR_INCLUDE}");
+    println!("cargo:rerun-if-env-changed={ENV_VAR_LIB}");
+
     println!("cargo:rerun-if-changed=src/lib.rs");
     println!("cargo:rerun-if-changed=cpp/cxx_api.hpp");
     println!("cargo:rerun-if-changed=cpp/cxx_api.cpp");
-    println!("cargo:rustc-link-search=native={exec_dir}/cmake-out/lib");
+    println!("cargo:rustc-link-search=native={lib_dir}");
     println!("cargo:rustc-link-lib=static:+whole-archive=executorch");
     println!("cargo:rustc-link-lib=static:+whole-archive=executorch_no_prim_ops");
     println!("cargo:rustc-link-lib=static:+whole-archive=extension_data_loader");
